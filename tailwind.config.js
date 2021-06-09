22efab42d9179fbbed2f3059455d52b391bc70ae
Yaman KATBY <m.yaman.katby@gmail.com>
2021-06-09 16:53:13 +0300

    🚧 Work on the Footer component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      serif: "Recoleta, sans-serif",
+      sans: "Raleway, sans-serif",
+    },
     extend: {
       colors: {
         primary: "#6C5BF7",
