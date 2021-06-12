5112b0413c922e81c3bb736a777294e8de4b9333
Yaman KATBY <m.yaman.katby@gmail.com>
2021-06-12 15:25:39 +0300

    🚧 Work on the Home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,49 @@
 import Layout from "../Layout";
 
 function Home() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <div className="mx-6 md:mx-8 lg:mx-10 xl:mx-14 mb-6 md:mb-8 lg:mb-10 xl:mb-14">
+        <header>
+          <h1 className="text-4xl sm:text-7xl font-serif font-medium">
+            Full-stack web and Mobile apps developer.
+          </h1>
+          <p className="mt-6 text-2xl text-primary font-serif">
+            #javascript #react #reactnative #ios #android #aspnet
+          </p>
+        </header>
+        <p className="text-xl sm:text-2xl mt-7 sm:mt-14">
+          I'm a full-stack web & mobile apps developer with over 7 years of
+          working experience. Currently working on a bunch of productivity-rated
+          apps that are used by thousands of professionals worldwide. Also, I
+          share coding tutorial videos on my YouTube channel.
+        </p>
+
+        <div className="flex mt-7 sm:mt-14">
+          <a href="#">
+            <span className="hidden sm:inline text-2xl text-text">
+              LinkedIn
+            </span>
+            <img src="/linkedin.svg" />
+          </a>
+          <a href="#" className="ml-8">
+            <span className="hidden sm:inline text-2xl text-text">GitHub</span>
+            <img src="/github.svg" />
+          </a>
+          <a href="#" className="ml-8">
+            <span className="hidden sm:inline text-2xl text-text">
+              Dribbble
+            </span>
+            <img src="/dribbble.svg" />
+          </a>
+          <a href="#" className="ml-8">
+            <span className="hidden sm:inline text-2xl text-text">YouTube</span>
+            <img src="/youtube.svg" />
+          </a>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 export default Home;
